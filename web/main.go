6be6f51c50804f8829e1ea485e9604726b73daab
ever <unix@vip.qq.com>
2024-08-30 16:55:06 +0800

    feat: app use swagger

--- web/main.go
+++ web/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/everpan/mdmg/v8runtime"
 	"github.com/everpan/mdmg/web/handler"
 	"github.com/gofiber/contrib/fiberzap/v2"
+	"github.com/gofiber/contrib/swagger"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 )
@@ -17,6 +18,9 @@ func main() {
 	app.Use(fiberzap.New(fiberzap.Config{
 		Logger: logger,
 	}))
+
+	swgCfg := swagger.Config{FilePath: "./docs/swagger.json", Path: "./swagger"}
+	app.Use(swagger.New(swgCfg))
 
 	apiRouter := app.Group("/api")
 	apiRouter.Group(handler.ICoderHandler.Path, handler.ICoderHandler.Handler)
